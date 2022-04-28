6461f596cfb7c1706b18aa7da39bf3363f64ef79
DEV <dev@dev.com>
2022-04-28 21:10:09 +0530

    commit..

--- 5 Javascript ADV/callbacks.js
+++ 5 Javascript ADV/callbacks.js
@@ -6,7 +6,7 @@
 //    console.log(i);
 // }
 
-// async 
+// async
 // setTimeout after 5 seconds calls the callMe
 // console.log("Timeout *************************************************")
 // setTimeout(function callMe(){
@@ -25,14 +25,32 @@
 //    console.log(i);
 // }
 // },5000)
-// var i = 0;
-for(var i = 0; i< 10; i+=2){
-   // block scoped variable
-   const iVal = i;
-   setTimeout(() => { console.log(iVal)},1000);
+var i = 0;
+while (i < 10) {
+  // block scoped variable
+  const iVal = i;
+  setTimeout(() => {
+    console.log(iVal);
+  }, i * 500);
+  i = i + 2;
 }
-for(var i = 1; i< 10; i+=2){
-   // block scoped variable
-   const iVal = i;
-   setTimeout(() => { console.log(iVal)},1000);
+var i = 1;
+while (i < 10) {
+  // block scoped variable
+  const iVal = i;
+  setTimeout(() => {
+    console.log(iVal);
+  }, i * 1000);
+  i = i + 2;
 }
+
+// storing function in a variable
+var functionVar = function (a, b) {
+  console.log(a);
+  console.log(b);
+  result = a + b;
+  console.log(result);
+  console.log("************");
+};
+
+functionVar(1, 2);
