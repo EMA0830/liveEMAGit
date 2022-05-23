508a358a9d37e41becebe1e46424ae2634d61a67
DEV <dev@dev.com>
2022-05-23 21:25:58 +0530

    commit..

--- 9 HIBERNATE/d-hibernate-crud-nomvn/src/com/demo/App.java
+++ 9 HIBERNATE/d-hibernate-crud-nomvn/src/com/demo/App.java
@@ -1,7 +1,9 @@
 package com.demo;
 
+import java.util.List;
 import java.util.Properties;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -11,6 +13,7 @@ import org.hibernate.service.ServiceRegistryBuilder;
 
 import com.demo.entity.Product;
 
+
 public class App {
 	public static void main(String[] args) {
 		SessionFactory sessionFactory = getSessionFactory();
@@ -18,12 +21,39 @@ public class App {
 		Session session = sessionFactory.openSession();
 		// begin tran
 		Transaction transaction = session.beginTransaction();
+		
 //		create entity
-		Product product = new Product();
-		product.setProductId("P02");
-		product.setProductName("ANGULAR");
-		product.setPrice(500);
-		session.save(product);
+//		Product product = new Product();
+//		product.setProductId("P02");
+//		product.setProductName("ANGULAR");
+//		product.setPrice(500);
+//		session.save(product);
+		
+//		product = new Product();
+//		product.setProductId("P03");
+//		product.setProductName("java");
+//		product.setPrice(500);
+//		session.save(product);
+		
+	// update
+//	Product product2 =	(Product)session.get(Product.class, "P02");
+//	product2.setProductName("spring");
+//	product2.setPrice(5000);
+//	session.update(product2);
+	
+		//search
+//		Product product2 =	(Product)session.get(Product.class, "P02");
+//		System.err.println(product2);
+		
+		//delete product
+//		Product product2 =	(Product)session.get(Product.class, "P03");
+//		session.delete(product2);
+		
+//		get all
+		Query query = session.createQuery("from Product");
+		List<Product> productsList = query.list();
+		productsList.forEach(System.out::println);
+		
 //      commit 
 		transaction.commit();
 		// close
