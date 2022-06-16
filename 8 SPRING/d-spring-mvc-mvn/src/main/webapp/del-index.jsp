7907807368d406b4f08e40dbe7aeda4bd0795cc9
DEV <dev@dev.com>
2022-06-16 22:08:51 +0530

    commit..

---
+++ 8 SPRING/d-spring-mvc-mvn/src/main/webapp/del-index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+	<h2>Hello World!</h2>
+	<%
+	for (int i = 0; i < 5; i++) {
+		out.println("<h1>" + i + "</h1>");
+	}
+	%>
+</body>
+</html>
--- 91-spring-boot/guide/dmp-sb-ng-rest-jpa-hib-crud.sql
+++ 91-spring-boot/guide/dmp-sb-ng-rest-jpa-hib-crud.sql
@@ -1,2 +1,13 @@
 CREATE DATABASE SBDB;
 use SBDB;
+insert into employees values (1,"r@r.com","rambo","john");
+insert into employees values (2,"r2@r.com","rambo2","john2");
+select * from employees;
+
+
+CREATE DATABASE PRODUCTSDB;
+USE PRODUCTSDB;
+SELECT * FROM PRODUCTS;
+
+CREATE DATABASE SBDB1;
+use SBDB1;
